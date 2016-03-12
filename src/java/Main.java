cf3ade846fe87ecf9cacabee7e28d0455680e76b
mikhail24 <kuzyi1919@gmail.com>
2016-03-12 13:53:18 +0300

    add new method

--- src/java/Main.java
+++ src/java/Main.java
@@ -7,6 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        //lol
+
+        print();
+    }
+
+    private static void print() {
+        System.out.println("Hi");
     }
 }
